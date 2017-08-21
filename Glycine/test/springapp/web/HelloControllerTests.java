da8420b213b26986b511c083923d98d65e4d27d1
ltyson1 <ltyson1@UL97909.uspto.gov>
2017-08-21 10:56:50 -0400

    Improved the controller

--- Glycine/test/springapp/web/HelloControllerTests.java
+++ Glycine/test/springapp/web/HelloControllerTests.java
@@ -1,5 +1,7 @@
 package springapp.web;
 
+import java.util.Map;
+
 import org.springframework.web.servlet.ModelAndView;
 
 import springapp.web.HelloController;
@@ -8,9 +10,13 @@ import junit.framework.TestCase;
 
 public class HelloControllerTests extends TestCase {
 
-    public void testHandleRequestView() throws Exception{		
+    public void testHandleRequestView() throws Exception{
         HelloController controller = new HelloController();
-        ModelAndView modelAndView = controller.handleRequest(null, null);		
-        assertEquals("hello.jsp", modelAndView.getViewName());
+        ModelAndView modelAndView = controller.handleRequest(null, null);
+        assertEquals("WEB-INF/jsp/hello.jsp", modelAndView.getViewName());
+        Map<String, Object> model = modelAndView.getModel();
+		assertNotNull(model);
+        String nowValue = (String) model.get("now");
+        assertNotNull(nowValue);
     }
 }
