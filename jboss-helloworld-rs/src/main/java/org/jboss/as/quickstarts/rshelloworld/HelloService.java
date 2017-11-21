8f2ad34b14818bd46fcdd09cfb35c2ed76278153
Leonard Tyson <ljtyson@gmail.com>
2017-11-20 22:39:02 -0500

    need to merge

--- jboss-helloworld-rs/src/main/java/org/jboss/as/quickstarts/rshelloworld/HelloService.java
+++ jboss-helloworld-rs/src/main/java/org/jboss/as/quickstarts/rshelloworld/HelloService.java
@@ -24,8 +24,4 @@ package org.jboss.as.quickstarts.rshelloworld;
  */
 public class HelloService {
 
-    String createHelloMessage(String name) {
-        return "Hello " + name + "!";
-    }
-
 }
--- jboss-helloworld-rs/src/main/java/org/jboss/as/quickstarts/rshelloworld/HelloWorld.java
+++ jboss-helloworld-rs/src/main/java/org/jboss/as/quickstarts/rshelloworld/HelloWorld.java
@@ -1,51 +1,63 @@
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2014, Red Hat, Inc. and/or its affiliates, and individual
- * contributors by the @authors tag. See the copyright.txt in the
- * distribution for a full listing of individual contributors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 package org.jboss.as.quickstarts.rshelloworld;
 
-import javax.inject.Inject;
+import java.util.Date;
+import java.util.HashMap;
+
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
-/**
- * A simple REST service which is able to say hello to someone using HelloService Please take a look at the web.xml where JAX-RS
- * is enabled
- * 
- * @author gbrey@redhat.com
- * 
- */
+
 
 @Path("/")
 public class HelloWorld {
-    @Inject
-    HelloService helloService;
+
 
     @GET
     @Path("/json")
     @Produces({ "application/json" })
     public String getHelloWorldJSON() {
-        return "{\"result\":\"" + helloService.createHelloMessage("World") + "\"}";
+        return "{\"result\":\"" + "Hello " + "World" + "!" + "\"}";
     }
 
     @GET
     @Path("/xml")
     @Produces({ "application/xml" })
     public String getHelloWorldXML() {
-        return "<xml><result>" + helloService.createHelloMessage("World") + "</result></xml>";
+        return "<xml><result>" + "Hello " + "World" + "!" + "</result></xml>";
     }
+
+	@GET
+	@Path("/autoqe")
+	@Produces(MediaType.APPLICATION_JSON) 
+	public String sendAutoQCRespose(
+			   @DefaultValue("Y") @QueryParam("loadStatus") String loadStatus,
+			   @DefaultValue("Default reason") @QueryParam("statusReason") String statusReason
+			) {
+	    HashMap<String, String> AutoQCMap = new HashMap<String, String>(); 
+	    Date date = new Date();
+		AutoQCMap.put("load_date", date.toString());
+	    //String loadStatus = ""; 
+		AutoQCMap.put("load_status", loadStatus);
+	    //String statusReason = "";
+		AutoQCMap.put("status_reason", statusReason);
+	    boolean goodQC = false;
+	    // posting to AutoQC if there is no doc type supported by SCORE
+	    //ConfirmationXML confirmXML = new ConfirmationXML(AutoQCMap);
+	    System.out.println("AUTOQC>>>Confirmation XML>>>" + "confirmXML.getXML()");
+	    try {
+
+	    } catch (Exception e) {
+	        System.out.println("AUTOQC>>>EFSLoadMethod>>>Could not post AutoQC>>>" + e.getMessage());
+	        e.printStackTrace();
+	    }
+
+	    String string = "{\"result\":\"" + "Hello " + goodQC + "!" + "\"}";
+		return string;
+	}
 
 }
